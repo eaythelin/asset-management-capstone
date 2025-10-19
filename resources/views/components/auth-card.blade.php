116769705c9a96797261155b4920c6b9dc156fb0
Yuiheron <guerrerojnmh@gmail.com>
2025-10-19 17:12:25 +0800

    changed the background color in auth page

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,4 +1,4 @@
-<div class = "min-h-screen flex items-center justify-center px-4 bg-gradient-to-br from-blue-900 via-blue-800 to-blue-700">
+<div class = "min-h-screen flex items-center justify-center px-4 bg-gradient-to-br from-blue-900 via-blue-500 to-yellow-600">
   <div class = "card p-6 w-full max-w-sm shadow-2xl bg-base-100 border-t-5 border-b-5 border-yellow-400 rounded-lg">
     <div class = "card-body w-full">
       <div class = "text-center p-2">
